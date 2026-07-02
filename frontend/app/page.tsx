de4f6a05d7648037cb4c259909d23ebff22f5429
unvi <149414969+Unstvii@users.noreply.github.com>
2026-07-02 22:28:56 +0300

    feat: add hero title and description

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,43 @@
-import LoginForm from "@/components/Auth/LoginForm";
-import Header from "@/components/Header/Header";
-import Image from "next/image";
-
-export default function Home() {
-  return <div>Main page!</div>;
-}
+const Home = () => {
+  return (
+    <div
+      className="h-screen w-100% pt-16 pb-4"
+      style={{
+        background: "var(--color-bg-base)",
+      }}
+    >
+      <div className="flex justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex-column w-100">
+          <h3
+            className="text-xs font-semibold uppercase tracking-wider font-mono mb-[14]"
+            style={{ color: "var(--color-text-accent)" }}
+          >
+            // Гейміфікована платформа для розробників
+          </h3>
+          <h1
+            className="text-5xl font-bold font-[Arial] mb-[22]"
+            style={{ color: "var(--color-text-primary)" }}
+          >
+            Прокачуй код, як{" "}
+            <span className="bg-gradient-to-r from-violet-500 via-fuchsia-500 to-pink-500 bg-clip-text text-transparent">
+              персонажа.
+            </span>
+          </h1>
+          <p className="mb-8" style={{ color: "white" }}>
+            DevQuest - це не готовий список завдань. Ти сам формулюєш квест, сам
+            призначаєш йому ціну в XP і сам вирішуєш, яка ачівка чекає в кінці.
+            Платформа лише рахує очки й показує рейтинг
+          </p>
+          <div className="mb-[38]">
+            <button className="">Створити свій квест</button>
+            <button className="">Подивитись рейтинг</button>
+          </div>
+        </div>
+        <div className="" style={{ background: "white" }}>
+          2
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
