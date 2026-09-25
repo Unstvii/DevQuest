3a8bb8efc3caf9ffbbc3bc4d78750b8f525f274a
unvi <149414969+Unstvii@users.noreply.github.com>
2026-09-25 10:05:52 +0300

    fix: build

--- frontend/services/authGuard/authGuard.tsx
+++ frontend/services/authGuard/authGuard.tsx
@@ -3,7 +3,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/store/auth/auth.store";
-import AuthSkeleton from "@/components/AuthSkeleton/AuthSkeleton";
 const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
